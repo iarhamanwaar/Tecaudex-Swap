a881d7ee941b64a9d248416f096f3923ffaa6d08
iarhamanwaar <arhamanwaar@gmail.com>
2022-08-06 00:42:04 +0500

    completed smart contract

--- src/contracts/TecaudexSwap.sol
+++ src/contracts/TecaudexSwap.sol
@@ -7,7 +7,9 @@ contract TecaudexSwap {
     Token public token;
     uint public rate = 50;
 
-    event TokenPurchased(address account, address token, uint amount, uint rate);
+    event TokensPurchased(address account, address token, uint amount, uint rate);
+
+    event TokensSold(address account, address token, uint amount, uint rate);
 
     constructor(Token _token) public {
         token = _token;
@@ -20,6 +22,19 @@ contract TecaudexSwap {
 
         token.transfer(msg.sender, tokenAmount);
 
-        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+        emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(uint _amount) public {
+        require(token.balanceOf(msg.sender) >= _amount);
+
+        uint etherAmount = _amount / rate;
+
+        require(address(this).balance >= etherAmount);
+
+        token.transferFrom(msg.sender, address(this), _amount);
+        msg.sender.transfer(etherAmount);
+
+        emit TokensSold(msg.sender, address(token), _amount, rate);
     }
 }
--- test/TecaudexSwap.test.js
+++ test/TecaudexSwap.test.js
@@ -64,4 +64,36 @@ contract('TecaudexSwap', ([deployer, investor]) => {
             assert.equal(event.rate.toString(), '50');
         })
     })
+
+    describe('sellTokens()', async () => {
+        let result;
+
+        before(async () => {
+            await token.approve(contract.address, tokens('50'), { from: investor });
+            result = await contract.sellTokens(tokens('50'), { from: investor });
+        })
+        
+        it('allows to sell', async () => {
+            // investor balance should be zero
+            let investorBalance = await token.balanceOf(investor);
+            assert.equal(investorBalance.toString(), tokens('0'));
+
+            // contract token balance should be 1 mil
+            let tecaudexSwapBalance = await token.balanceOf(contract.address);
+            assert.equal(tecaudexSwapBalance.toString(), tokens('1000000'));
+
+            // eth balance in contract should be 0
+            tecaudexSwapBalance = await web3.eth.getBalance(contract.address);
+            assert.equal(tecaudexSwapBalance.toString(), tokens('0'));
+
+            const event = result.logs[0].args;
+            assert.equal(event.account, investor);
+            assert.equal(event.token, token.address);
+            assert.equal(event.amount.toString(), tokens('50').toString());
+            assert.equal(event.rate.toString(), '50');
+
+            // investor cant sell more tokens than they have
+            await contract.sellTokens(tokens('500'), { from: investor }).should.be.rejected;
+        })
+    })
 })
